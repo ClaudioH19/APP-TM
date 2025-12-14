0e1baf42fea34ce6becda52b287e6381d0461631
ClaudioH19 <clhernandez21@alumnos.utalca.cl>
2025-12-14 19:09:13 -0300

    cambio permision

--- App/babel.config.js
+++ App/babel.config.js
@@ -2,7 +2,7 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo', 'nativewind/babel'],
-    plugins: [
+    plugins: ['react-native-reanimated/plugin'
     ],
   };
 };
